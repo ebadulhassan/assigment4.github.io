e271c89b9182da3b666f1a2f64af649bd63e810e
ebadulhassan <ebadulhassan967@gmail.com>
2023-05-21 14:48:05 -0700

    assigment4

---
+++ index.js
@@ -0,0 +1,18 @@
+// let x = prompt("enter first  value ");
+// if (x%2==0){
+//     document.write(x ,"this is even number")
+
+// }
+// else{
+//     document.write(x ,"this is odd number")
+// }
+
+
+
+let value1 = prompt("Enter the first number")
+if (value1%2==0){
+    document.write(value1,"this is even number")
+}
+else{
+    document.write(value1,"this is odd number")
+}
